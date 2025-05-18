18904b005b0149bf95384de95b6f466d758723b6
renzcuison <rbboy099@gmail.com>
2025-05-19 00:03:31 +0800

    fixed

--- frontend/src/views/Stocks/Release.vue
+++ frontend/src/views/Stocks/Release.vue
@@ -394,13 +394,8 @@ function saveStock(username) {
           price_per_unit: stock.price_per_unit,
           buying_price: stock.buying_price || 0,
           description: stock.description || '',
-<<<<<<< HEAD
           date_released: dateReleased.value,
           receiver: receiverName.value
-=======
-          date_released: this.dateReleased,
-          receiver: this.receiverName,
->>>>>>> 3dba2cdf39cbfc8e1fb59e8ee1ed214c311fcae7
         };
 
         return axios.put(`http://localhost:8001/api/stocks/${stock.stock_id}`, stockPayload);
